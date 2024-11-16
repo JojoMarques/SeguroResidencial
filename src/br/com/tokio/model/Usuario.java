d1cbafdfa58376966363e4ceef6860c75413b2cb
jpfelixx <felix.joao@aluno.ifsp.edu.br>
2024-11-16 17:07:30 -0300

    update

---
+++ src/br/com/tokio/model/Usuario.java
@@ -0,0 +1,14 @@
+package br.com.tokio.model;
+
+public class Usuario {
+	    private int idUsuario;
+	    private String nome;
+	    private String cpf;
+	    private String telefone;
+	    private String email;
+	    
+	
+	
+	    
+	    
+}
