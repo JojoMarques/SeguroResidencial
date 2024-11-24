fe856036936312fa264977fb3f89f276d7258f0a
Marisa Mayumi Morita Ouchi <126785489+maris2606@users.noreply.github.com>
2024-11-24 15:50:12 -0300

    Update LoginFuncionario.java

--- src/br/com/tokio/view/funcionario/LoginFuncionario.java
+++ src/br/com/tokio/view/funcionario/LoginFuncionario.java
@@ -145,7 +145,7 @@ public class LoginFuncionario {
 		JTextPane txtpnRestritoAosCorretores = new JTextPane();
 		txtpnRestritoAosCorretores.setBackground(UIManager.getColor("Button.background"));
 		txtpnRestritoAosCorretores.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtpnRestritoAosCorretores.setText("Restrito aos corretores / colaboradores e parceiros.");
+		txtpnRestritoAosCorretores.setText("Restrito aos corretores / colaboradores.");
 		txtpnRestritoAosCorretores.setBounds(10, 24, 383, 20);
 		panel.add(txtpnRestritoAosCorretores);
 
